5c21074729f255443602ebff4e2d4a291e2c9feb
KedarMatkar <akari.ikeda21@gmail.com>
2024-05-01 18:18:21 +0530

    first commit

---
+++ AddNumbers.java
@@ -0,0 +1,8 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.print("sum:" + sum);
+    }
+}
